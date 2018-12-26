a4742fd7540958f10d68d3e2d41111fab5963ede
pahalce <tkrmineral@gmail.com>
2018-12-27 00:25:50 +0900

    addded index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<title>YAMANOTE-UNTIn</title>
+	<meta name="viewport" content="width=device-width">　　　  
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	<script src="json/Stationlist"></script>	
+	<div id="header">
+		<h1>YAMANOTE-<span>UNTI</span>n</h1>
+	</div>
+	<div id="contents">
+		<select>
+			<option id="stationsPulldown"></option>
+	        </select>
+	</div>
+</body>
+</html>
+
+
